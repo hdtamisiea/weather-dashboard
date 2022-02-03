f18ae67b7f4d760bf2d7cc6dace012c4db82e349
htam <hdfair@gmail.com>
2022-02-03 12:57:54 -0500

    set to local storage and loads from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // Create global variables
 var APIKey = "95f4113ff118dc3d9eecf2014570612e";
 var cityFormEl = document.querySelector("#city-form");
-var cityInputEl = document.querySelector("#cityName");
+var cityInputEl = document.querySelector("#city-name");
 var dailyBox = document.getElementById("daily-box");
+var saveCityArr = JSON.parse(localStorage.getItem("cities")) || [];
 
 
 // added moment.js to get current date as well as dates for 5 day forecast
@@ -15,6 +16,7 @@ var date5 = moment().add(5, 'days').format("MMM Do, YYYY");
 
 // created array for forecast dates
 let forecastDates = [date1, date2, date3, date4, date5];
+
 
 // Function to get 5 day forecast
 var getFiveDayStats = function (cityName) {
@@ -123,13 +125,13 @@ var getCityStats = function (cityName) {
                   var currentDayIcon = document.querySelector(".current-day-icon");
                   currentDayIcon.setAttribute('src', iconUrl);
 
-                  
-              // calling the 5 day forecast function
-              getFiveDayStats(cityName)
+
+                  // calling the 5 day forecast function
+                  getFiveDayStats(cityName)
+                })
             })
-        })
+        });
     });
-});
 };
 
 // Create a new button for every new city entered, this will be saved to local storage. User can either type in a new city, or choose a recent search.
@@ -139,6 +141,10 @@ var createRecent = function (cityName) {
 
   // Set text content of recent search button and change to uppercase for appearances
   newButton.textContent = cityName.toUpperCase();
+  
+
+  localStorage.setItem("cities", JSON.stringify(saveCityArr));
+
 
   // Event listener to get weather if a previous search button is clicked
   newButton.addEventListener("click", function (event) {
@@ -150,28 +156,146 @@ var createRecent = function (cityName) {
   newButton.setAttribute("class", "button btn-primary btn-block flex-row");
 }
 
-// function to gather city name from user and display today's weather stats
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-  // get value from input element
-  var cityName = cityInputEl.value.trim();
+  //   function saveSearchHistory(cityName) {
+  //     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  //     searchHistory.push(cityName);
+  //     searchHistory = searchHistory.filter(function(value, index, array) {
+  //         return array.indexOf(value) === index
+  //     });
+  //     if (searchHistory.length > 10) {
+  //         searchHistory = searchHistory.slice(1, 11);
+  //     }
+  //     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  //     loadSearchHistory();
+  // }
 
-  // If a city name is entered create a recent search button and run function to retrieve today's weather stats
-  if (cityName) {
-    createRecent(cityName);
-    getCityStats(cityName);
-    // reset box to be empty
-    cityInputEl.value = "";
+  // function loadSearchHistory() {
+  //       var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  //       searchHistoryListEl.innerHTML = "";
 
-    // If no city name is entered, alert user that input is required
-  } else {
-    alert("Please enter a City Name");
+  //       for (let i = 0; i < searchHistory.length; i++) {
+  //           var searchHistoryListItemEl = document.createElement("li");
+  //           searchHistoryListItemEl.textContent = searchHistory[i];
+
+  //           searchHistoryListEl.prepend(searchHistoryListItemEl);
+  //       }
+  //   }
+
+  // }
+
+
+  // function to gather city name from user and display today's weather stats
+  var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // get value from input element
+    var cityName = cityInputEl.value.trim();
+
+    // If a city name is entered create a recent search button and run function to retrieve today's weather stats
+    if (cityName) {
+      saveCityArr.push(cityName)
+      createRecent(cityName);
+      getCityStats(cityName);
+     
+      // reset box to be empty
+      cityInputEl.value = "";
+
+      // If no city name is entered, alert user that input is required
+    } else {
+      alert("Please enter a City Name");
+    }
+    console.log(event);
+
+    // saveCity();
+    // getSavedCities();
+  };
+
+  cityFormEl.addEventListener("submit", formSubmitHandler);
+
+  for (let i = 0; i < saveCityArr.length; i++) {
+    createRecent(saveCityArr[i]);
+    console.log("hello")
   }
-  console.log(event);
-};
+
+// var newCityArray = [];
+// var size = 5;
+
+// for (var i = 0; i < size; i++) {
+//   newCityArray[i] = (cityName + (i + 1));
+// }
+
+// function saveSearchHistory(cityName) {
+//       var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+//       searchHistory.push(cityName);
+//       searchHistory = searchHistory.filter(function(value, index, array) {
+//           return array.indexOf(value) === index
+//       });
+//       if (searchHistory.length > 10) {
+//           searchHistory = searchHistory.slice(1, 11);
+//       }
+//       localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+//       loadSearchHistory();
+//   }
+
+//   function loadSearchHistory() {
+//         var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+//         searchHistoryListEl.innerHTML = "";
+
+//         for (let i = 0; i < searchHistory.length; i++) {
+//             var searchHistoryListItemEl = document.createElement("li");
+//             searchHistoryListItemEl.textContent = searchHistory[i];
+
+//             searchHistoryListEl.prepend(searchHistoryListItemEl);
+//         }
+//     }
+
+// function saveArray() {
+//   var savedCities = [];
+//   for (var i = 0; i < savedCities.length; i++) {
+//     cityInputEl(savedCities[i]);
+//     localStorage.setItem("savedCities", JSON.stringify(cityInputEl.value));
+//   }
+// }
+
+
+
+//   var newCity = {};
+//   $(`.cityName`).each(function() {
+//     newCity[this.id] = this.value;
+//   })
+//   console.log(localStorage.getItem("savedCity"));
+//   console.log(JSON.parse(localStorage.getItem("savedCity")));
+// }
+//   var cityName = cityInputEl.value.trim();
+//   var newCity = JSON.stringify(cityName);
+//   localStorage.setItem("cityName", newCity);
+
+//   var getStoredCities = localStorage.getItem("cityName");
+
+// };
+
+
+
+// function getSavedCities() {
+//   var newCity = localStorage.getItem("newCity");
+//   document.getElementById("")
+// }
+
+// var loadCities = function() {
+//   var savedCities = localStorage.getItem("cityName");
+//   if (!savedCities) {
+//     return false;
+//   } 
+//   savedCities = JSON.parse(savedCities);
+
+//   for (var i = 0; i < savedCities.length; i++) {
+//     cityInputEl(savedCities[i]);
+//   }
+// };
+
+// loadCities();
 
 // Once a city is entered and "Get Weather" is clicked, run formSubmitHandler
-cityFormEl.addEventListener("submit", formSubmitHandler);
+
 
 // set new submission to local storage
-// localStorage.setItem("cityName", JSON.stringify(cityName));
+// localStorage.setItem("cityName", JSON.stringify(cityName))
